eb5089048e27b02d6314aeb833abfb636fdd0ffa
Sebastian Menski <sebastian.menski@googlemail.com>
2012-07-07 16:40:42 +0200

    linda salbnet; extend implementation; improve listings

--- doc/master/listings/client_report.c
+++ doc/master/listings/client_report.c
@@ -1,47 +1,27 @@
-/*
- * update metrics and credits
- */
-  static int
-client_report_update(struct client_context * context, struct client_network * network)
-{
-  int  result;
-  int  packet_median;
+#define UDP_TRUESIZE(X) (((int) (((X) + 63) / 128)) * 128 + 376)
 
-  /* update metrics */
-  result = client_metric_update(context, network);
-  if (result == ERROR) {
-    syslog(LOG_ERR, "could not update metrics (%s at line %d)", __FILE__, __LINE__);
-    return ERROR;
-  }
-
-  /* calculate credits from metrics */
-  switch (network->protocol) {
-    case IPPROTO_TCP:
-      /* ... */
-      break;
-    case IPPROTO_UDP:
-      packet_median = UDP_TRUESIZE(network->metrics.udp[CLIENT_METRIC_UDP_RECEIVE_SIZE].history_median);
-      if (network->metrics.udp[CLIENT_METRIC_UDP_DROP].history[1] > 0 &&
-          network->metrics.udp[CLIENT_METRIC_UDP_DROP].current - network->metrics.udp[CLIENT_METRIC_UDP_DROP].history[1] > 0) {
-        network->credits.hard = 0;
-        network->credits.soft = 0;
-      }
-      else {
-        network->credits.hard = (uint32_t) ((network->metrics.udp[CLIENT_METRIC_UDP_RECEIVE_QUEUE].maximum - network->metrics.udp[CLIENT_METRIC_UDP_RECEIVE_QUEUE].current) / packet_median); 
-        network->credits.soft = (uint32_t) ((network->metrics.udp[CLIENT_METRIC_UDP_RECEIVE_QUEUE].maximum - network->metrics.udp[CLIENT_METRIC_UDP_RECEIVE_QUEUE].history_median) / packet_median); 
-      }
-      break;
-  }
-
-  return SUCCESS;
-}
-
-/* calculate maximum and current */
 switch (network->protocol) {
   case IPPROTO_TCP:
-    maximum = network->metrics.tcp[CLIENT_METRIC_TCP_SYN_RECEIVED].maximum + network->metrics.tcp[CLIENT_METRIC_TCP_ACKNOWLEDGED].maximum;
-    current = network->metrics.tcp[CLIENT_METRIC_TCP_SYN_RECEIVED].current + network->metrics.tcp[CLIENT_METRIC_TCP_ACKNOWLEDGED].current;
+    /* ... */
     break;
+  case IPPROTO_UDP:
+    packet_median = UDP_TRUESIZE(network->metrics.udp[CLIENT_METRIC_UDP_RECEIVE_SIZE].history_median);
+    if (network->metrics.udp[CLIENT_METRIC_UDP_DROP].history[1] > 0 &&
+        network->metrics.udp[CLIENT_METRIC_UDP_DROP].current - network->metrics.udp[CLIENT_METRIC_UDP_DROP].history[1] > 0) {
+      network->credits.hard = 0;
+      network->credits.soft = 0;
+    }
+    else {
+      network->credits.hard = (uint32_t) ((network->metrics.udp[CLIENT_METRIC_UDP_RECEIVE_QUEUE].maximum - network->metrics.udp[CLIENT_METRIC_UDP_RECEIVE_QUEUE].current) / packet_median); 
+      network->credits.soft = (uint32_t) ((network->metrics.udp[CLIENT_METRIC_UDP_RECEIVE_QUEUE].maximum - network->metrics.udp[CLIENT_METRIC_UDP_RECEIVE_QUEUE].history_median) / packet_median); 
+    }
+    break;
+}
+
+
+switch (network->protocol) {
+  case IPPROTO_TCP:
+    /* ... */
   case IPPROTO_UDP:
     maximum = (uint32_t) network->metrics.udp[CLIENT_METRIC_UDP_RECEIVE_QUEUE].maximum / UDP_TRUESIZE(network->metrics.udp[CLIENT_METRIC_UDP_RECEIVE_SIZE].maximum);
     current = (uint32_t) network->metrics.udp[CLIENT_METRIC_UDP_RECEIVE_QUEUE].current / UDP_TRUESIZE(network->metrics.udp[CLIENT_METRIC_UDP_RECEIVE_SIZE].history_median);
--- doc/master/listings/nethook.c
+++ doc/master/listings/nethook.c
@@ -1,51 +1,24 @@
 ssize_t
 recvmsg(int sockfd, struct msghdr *msg, int flags)
 {
-  static recvmsg_t libc_recvmsg;
-  static struct nethook_context context;
-  struct nethook_shared *shared;
-  pid_t           pid;
-  struct nethook_data *data;
-  int             result;
-  ssize_t		libc_result;
-
-  /* set shared memory */
-  shared = context.shared;
-
+  /* ... */
   /* set libc recvmsg */
   if (libc_recvmsg == NULL) {
     /* set libc recvmsg */
     libc_recvmsg = (recvmsg_t) dlsym(RTLD_NEXT, "recvmsg");
     if (libc_recvmsg == NULL) {
-      syslog(LOG_ERR, "failed to find libc recvmsg symbol: %s (%s at line %d)", dlerror(), __FILE__, __LINE__);
-      return ERROR;
+      /* ... ERROR */
     }
   }
-
   /* ... */
-
   /* call libc recvmsg */
   libc_result = libc_recvmsg(sockfd, msg, flags);
   if (libc_result == ERROR) {
-    syslog(LOG_ERR, "recvmsg error: %s", strerror(errno));
-    return libc_result;
+    /* ... ERROR */
   }
-
   /* ... */
-
-        /* set current data pointer */
-        data = &shared->data[shared->data_count];
-        memset(data, 0, sizeof(struct nethook_data));
-
-  /* ... */
-
         /* set recv size */
         data->recv_size = libc_result;
-
-        /* increment data count */
-        shared->data_count++;
-
   /* ... */
-
   return libc_result;
 }
