f79f462ced1d0a7fe08986b406a8c80a73658796
Sebastian Menski <sebastian.menski@googlemail.com>
2012-07-07 17:49:15 +0200

    finish implementation

--- doc/master/listings/client_report.c
+++ doc/master/listings/client_report.c
@@ -1,29 +1,16 @@
 #define UDP_TRUESIZE(X) (((int) (((X) + 63) / 128)) * 128 + 376)
 
-switch (network->protocol) {
-  case IPPROTO_TCP:
-    /* ... */
-    break;
-  case IPPROTO_UDP:
-    packet_median = UDP_TRUESIZE(network->metrics.udp[CLIENT_METRIC_UDP_RECEIVE_SIZE].history_median);
-    if (network->metrics.udp[CLIENT_METRIC_UDP_DROP].history[1] > 0 &&
-        network->metrics.udp[CLIENT_METRIC_UDP_DROP].current - network->metrics.udp[CLIENT_METRIC_UDP_DROP].history[1] > 0) {
-      network->credits.hard = 0;
-      network->credits.soft = 0;
-    }
-    else {
-      network->credits.hard = (uint32_t) ((network->metrics.udp[CLIENT_METRIC_UDP_RECEIVE_QUEUE].maximum - network->metrics.udp[CLIENT_METRIC_UDP_RECEIVE_QUEUE].current) / packet_median); 
-      network->credits.soft = (uint32_t) ((network->metrics.udp[CLIENT_METRIC_UDP_RECEIVE_QUEUE].maximum - network->metrics.udp[CLIENT_METRIC_UDP_RECEIVE_QUEUE].history_median) / packet_median); 
-    }
-    break;
+packet_median = UDP_TRUESIZE(network->metrics.udp[CLIENT_METRIC_UDP_RECEIVE_SIZE].history_median);
+if (network->metrics.udp[CLIENT_METRIC_UDP_DROP].history[1] > 0 &&
+    network->metrics.udp[CLIENT_METRIC_UDP_DROP].current - network->metrics.udp[CLIENT_METRIC_UDP_DROP].history[1] > 0) {
+  network->credits.hard = 0;
+  network->credits.soft = 0;
+}
+else {
+  network->credits.hard = (uint32_t) ((network->metrics.udp[CLIENT_METRIC_UDP_RECEIVE_QUEUE].maximum - network->metrics.udp[CLIENT_METRIC_UDP_RECEIVE_QUEUE].current) / packet_median);
+  network->credits.soft = (uint32_t) ((network->metrics.udp[CLIENT_METRIC_UDP_RECEIVE_QUEUE].maximum - network->metrics.udp[CLIENT_METRIC_UDP_RECEIVE_QUEUE].history_median) / packet_median);
 }
 
 
-switch (network->protocol) {
-  case IPPROTO_TCP:
-    /* ... */
-  case IPPROTO_UDP:
-    maximum = (uint32_t) network->metrics.udp[CLIENT_METRIC_UDP_RECEIVE_QUEUE].maximum / UDP_TRUESIZE(network->metrics.udp[CLIENT_METRIC_UDP_RECEIVE_SIZE].maximum);
-    current = (uint32_t) network->metrics.udp[CLIENT_METRIC_UDP_RECEIVE_QUEUE].current / UDP_TRUESIZE(network->metrics.udp[CLIENT_METRIC_UDP_RECEIVE_SIZE].history_median);
-    break;
-}
+maximum = (uint32_t) network->metrics.udp[CLIENT_METRIC_UDP_RECEIVE_QUEUE].maximum / UDP_TRUESIZE(network->metrics.udp[CLIENT_METRIC_UDP_RECEIVE_SIZE].maximum);
+current = (uint32_t) network->metrics.udp[CLIENT_METRIC_UDP_RECEIVE_QUEUE].current / UDP_TRUESIZE(network->metrics.udp[CLIENT_METRIC_UDP_RECEIVE_SIZE].history_median);
